01b8f1af46d30f2053f9855f26332c1f2b487327
Fernando Encinas <FernandoEncinas@Medigos-MacBook.local>
2019-08-14 09:10:47 +0200

    adding updates to branch

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ $(function() {
   var client = ZAFClient.init();
   client.invoke('resize', { width: '100%', height: '120px' });
   showInfo();
-  showError();
+  //showError();
 });
 
 function showInfo() {
